319471de33fcb776a6a1bea28377d9e24d688b33
MBlackP <permjotdhari@gmail.com>
2021-09-19 22:57:39 -0700

    add error boundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 // Smart component because it has state. Smart components tend to have the class syntax.
@@ -39,7 +40,9 @@ class App extends Component {
                         <h1>RoboFriends</h1>
                         <SearchBox searchChange={this.onSearchChange}/>
                         <Scroll>
-                            <CardList robots={filteredRobots}/>
+                            <ErrorBoundary>
+                                <CardList robots={filteredRobots}/>
+                            </ErrorBoundary>
                         </Scroll>
                     </div>
         );
