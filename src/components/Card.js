8e81b94368ded06c4ab6d1a0e050b8dc5a70d035
MBlackP <permjotdhari@gmail.com>
2021-09-20 16:25:39 -0700

    added open in new tab

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,15 +6,17 @@ const Card = ({name, username, ecosystem, id}) => {
     const twitterurl = `https:www.twitter.com/${username}`;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='pic' src={`https://robohash.org/${id}?200x200`}/>
-            <div>
-                <h2>{name}</h2>
-                <div >
-                    <img alt='twitter' className='twittericon' src='https://logos-world.net/wp-content/uploads/2020/04/Twitter-Logo.png'/>
-                    <a href={twitterurl}>{username}</a>
-                    <p className='ttu'>{ecosystem}</p>
+            <a href={twitterurl} target='_blank' rel='noreferrer' className='no-underline'>
+                <img alt='pic' src={`https://robohash.org/${id}?200x200`}/>
+                <div>
+                    <h2>{name}</h2>
+                    <div >
+                        <img alt='twitter' className='twittericon' src='https://logos-world.net/wp-content/uploads/2020/04/Twitter-Logo.png'/>
+                        <a href={twitterurl} target='_blank'>{username}</a>
+                        <p className='ttu'>{ecosystem}</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     );
 }
